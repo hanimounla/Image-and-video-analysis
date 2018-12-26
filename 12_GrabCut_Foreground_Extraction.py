f893e96de0d0901aedd931fb38b44e83bd3755e7
hani mounla <hani mounla>
2018-12-27 00:20:38 +0200

    added the grabCut to select my head and adjusted the positions

---
+++ 12_GrabCut_Foreground_Extraction.py
@@ -0,0 +1,19 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+img = cv2.imread('images/hani3.jpg')
+mask = np.zeros(img.shape[:2],np.uint8)
+
+bgdModel = np.zeros((1,65),np.float64)
+fgdModel = np.zeros((1,65),np.float64)
+
+rect = (240,5,200,280)
+
+cv2.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
+mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
+img = img*mask2[:,:,np.newaxis]
+
+plt.imshow(img)
+plt.colorbar()
+plt.show()
